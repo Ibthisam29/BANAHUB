6a788f395ee54783ad7d3c5b8a8e44e0b5bd560c
IBTHISAM <87207543+Ibthisam29@users.noreply.github.com>
2026-08-06 16:40:52 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  SUPABASE_URL: 'https://mfqdqisbryoepdpqebkb.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_GfOFDZkHqJKEuKTIS_iU_A_9ttmG7HS',
-  VERSION: '2.3.0',
-};
-window.CONFIG = CONFIG;
